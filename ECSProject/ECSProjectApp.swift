944d77745d6e928749e053239073bff5e1c105eb
RazielApple <fbarbato22@fed.idserve.net>
2023-01-11 16:25:14 +0100

    Initial Commit

---
+++ ECSProject/ECSProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECSProjectApp.swift
+//  ECSProject
+//
+//  Created by Fabio Barbato on 11/01/23.
+//
+
+import SwiftUI
+
+@main
+struct ECSProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
