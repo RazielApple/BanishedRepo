48a94c17bf29904288045e49c562657be90e49ef
RazielApple <fbarbato22@fed.idserve.net>
2023-01-11 16:30:16 +0100

    messaggi edit 2

--- ECSProject/ContentView.swift
+++ ECSProject/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Jng diff")
+            Text("jng diff")
         }
         .padding()
     }
